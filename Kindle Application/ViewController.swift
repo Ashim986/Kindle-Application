e7ef4eef808fadd17b85303e6d0c2798b032db72
Ashim986 <ashim44@gmail.com>
2018-06-12 00:44:07 -0500

    Initial Commit

---
+++ Kindle Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kindle Application
+//
+//  Created by ashim Dahal on 6/12/18.
+//  Copyright © 2018 ashim Dahal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
